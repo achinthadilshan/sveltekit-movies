1221f354083f39cc4a815ec2ca9dce41f4ddfa0f
ACHii <>
2023-07-26 23:24:33 +0530

    setup icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.css';
+	import NavBar from '../components/NavBar.svelte';
+	import Icon from '~icons/icon-park-solid/check-one';
 </script>
 
+<NavBar />
+<Icon class="text-xl" />
 <slot />
