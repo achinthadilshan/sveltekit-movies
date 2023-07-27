13e6c09942381348b627648b4c730cc6bfeb8254
ACHii <>
2023-07-28 01:12:22 +0530

    navbar

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,1 +1,23 @@
-<div>Test</div>
+<script>
+	import Icon from '~icons/tabler/movie';
+</script>
+
+<div class=" bg-slate-900 py-2">
+	<div class="container mx-auto flex items-center justify-between">
+		<a href="/" class="flex items-center">
+			<Icon class="text-4xl text-orange-500" />
+			<span class="ml-2 text-lg font-semibold text-white">Movie</span>
+			<span class="block rounded bg-orange-500 p-1 text-lg font-semibold leading-none text-white"
+				>Hub</span
+			>
+		</a>
+		<ul class="flex items-center gap-4">
+			<li>
+				<a href="/" class=" font-semibold text-white hover:text-orange-400">Home</a>
+			</li>
+			<li>
+				<a href="/" class=" font-semibold text-white hover:text-orange-400">About</a>
+			</li>
+		</ul>
+	</div>
+</div>
