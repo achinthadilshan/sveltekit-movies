1e737a63a1fb8071fbad359232bfbcefadd7bc5e
ACHii <>
2023-07-30 18:57:37 +0530

    update

--- src/components/SearchForm.svelte
+++ src/components/SearchForm.svelte
@@ -1,19 +1,27 @@
 <script>
 	import Icon from '~icons/ri/search-line';
+	import SearchResultsList from './SearchResultsList.svelte';
 </script>
 
 <form action="" class="w-full max-w-2xl">
-	<div class="flex input-wrapper items-center text-slate-200 w-full bg-transparent overflow-hidden rounded-lg border-2 border-slate-300">
-		<input type="text" class="bg-transparent w-full focus:outline-none leading-[48px] px-4 text-lg text-slate-200" placeholder="Search" />
-        <button type="submit" class="h-[48px] pr-3 bg-transparent">
-            <Icon class="text-xl text-inherit"/>
-        </button>
+	<div
+		class="input-wrapper mb-4 flex w-full items-center overflow-hidden rounded-lg border-2 border-slate-300 bg-transparent text-slate-200"
+	>
+		<input
+			type="text"
+			class="w-full bg-transparent px-4 text-lg leading-[48px] text-slate-200 focus:outline-none"
+			placeholder="Search"
+		/>
+		<button type="submit" class="h-[48px] bg-transparent pr-3">
+			<Icon class="text-xl text-inherit" />
+		</button>
 	</div>
+	<SearchResultsList />
 </form>
 
 <style>
-    .input-wrapper:has(input:focus) {
-        border: 2px solid #f97316;
-        color: #f97316;
-    }
+	.input-wrapper:has(input:focus) {
+		border: 2px solid #f97316;
+		color: #f97316;
+	}
 </style>
---
+++ src/components/SearchResultsList.svelte
@@ -0,0 +1,52 @@
+<script></script>
+
+<div class="rounded-lg bg-white/80 px-2 py-4">
+	<ul class="mr-4 max-h-80 w-full overflow-y-auto">
+		<li class=" border-b border-slate-200">
+			<a href="/" class="flex items-center space-x-4 py-2">
+				<img
+					src="https://picsum.photos/100/200"
+					class="h-16 w-12 object-cover"
+					alt="movie thumbnail"
+				/>
+				<span class="font-semibold text-slate-700">Spider-Man: Homecoming</span>
+			</a>
+		</li>
+		<li class=" border-b border-slate-200">
+			<a href="/" class="flex items-center space-x-4 py-2">
+				<img
+					src="https://picsum.photos/100/200"
+					class="h-16 w-12 object-cover"
+					alt="movie thumbnail"
+				/>
+				<span class="font-semibold text-slate-700">Spider-Man: Homecoming</span>
+			</a>
+		</li>
+		<li class=" border-b border-slate-200">
+			<a href="/" class="flex items-center space-x-4 py-2">
+				<img
+					src="https://picsum.photos/100/200"
+					class="h-16 w-12 object-cover"
+					alt="movie thumbnail"
+				/>
+				<span class="font-semibold text-slate-700">Spider-Man: Homecoming</span>
+			</a>
+		</li>
+		<li class=" border-b border-slate-200">
+			<a href="/" class="flex items-center space-x-4 py-2">
+				<img
+					src="https://picsum.photos/100/200"
+					class="h-16 w-12 object-cover"
+					alt="movie thumbnail"
+				/>
+				<span class="font-semibold text-slate-700">Spider-Man: Homecoming</span>
+			</a>
+		</li>
+	</ul>
+</div>
+
+<style>
+	ul {
+		scrollbar-gutter: stable both-edges;
+	}
+</style>
