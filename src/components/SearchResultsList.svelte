89450d004ebb0343301194c7e6cb3cd10d700ed7
ACHii <>
2023-08-03 21:48:33 +0530

    update

--- src/components/SearchResultsList.svelte
+++ src/components/SearchResultsList.svelte
@@ -1,7 +1,7 @@
 <script></script>
 
 <div class="top absolute left-0 top-16 w-full rounded-lg bg-white/80 px-2 py-4">
-	<ul class="mr-4 max-h-80 w-full overflow-y-auto">
+	<ul class="mr-4 max-h-[25vh] w-full overflow-y-auto">
 		<li class=" border-b border-slate-200">
 			<a href="/" class="flex items-center space-x-4 py-2">
 				<img
