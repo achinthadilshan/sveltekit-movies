1863925248830837ed7380985623a6cbc8298006
ACHii <>
2023-08-05 14:26:02 +0530

    update

--- src/components/SearchForm.svelte
+++ src/components/SearchForm.svelte
@@ -1,9 +1,57 @@
 <script>
 	import Icon from '~icons/ri/search-line';
+	import Loading from '~icons/line-md/loading-alt-loop';
 	import SearchResultsList from './SearchResultsList.svelte';
+
+	let search = '';
+	let movies = [];
+	let timeout;
+	let loading = false;
+
+	function handle_search() {
+		loading = true;
+		if (timeout) clearTimeout(timeout);
+		timeout = setTimeout(search_movies, 500);
+	}
+
+	async function search_movies() {
+		if (!search) {
+			reset();
+			return;
+		}
+
+		try {
+			const options = {
+				method: 'GET',
+				headers: {
+					accept: 'application/json',
+					Authorization:
+						'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWM0YWVkOTRjZTZkYmNlZmM5MjMxNGNiMDQ0ZjJhNiIsInN1YiI6IjY0YmU4YzkzNThlZmQzMDBmZjFjNWI3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TTpQR13CiMqKlsgXzMT37UKpleotexOfj8NzNYQ_2nA'
+				}
+			};
+
+			const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${search}`, options);
+			const data = await res.json();
+			movies = data?.results || [];
+			loading = false;
+		} catch (e) {
+			console.log(e);
+			reset();
+		}
+	}
+
+	function handleError() {
+		alert('Something went wrong.');
+		reset();
+	}
+
+	function reset() {
+		movies = [];
+		loading = false;
+	}
 </script>
 
-<form action="" class="relative w-full max-w-2xl">
+<div class="relative w-full max-w-2xl">
 	<div
 		class="input-wrapper flex w-full items-center overflow-hidden rounded-lg border-2 border-slate-300 bg-transparent text-slate-200"
 	>
@@ -11,13 +59,17 @@
 			type="text"
 			class="w-full bg-transparent px-4 text-lg leading-[48px] text-slate-200 focus:outline-none"
 			placeholder="Search"
+			bind:value={search}
+			on:input={handle_search}
 		/>
-		<button type="submit" class="h-[48px] bg-transparent pr-3">
-			<Icon class="text-xl text-inherit" />
-		</button>
+		{#if loading}
+			<Loading class="mx-3 text-2xl text-inherit" />
+		{:else}
+			<Icon class="mx-3 text-xl text-inherit" />
+		{/if}
 	</div>
-	<SearchResultsList />
-</form>
+	<SearchResultsList {movies} />
+</div>
 
 <style>
 	.input-wrapper:has(input:focus) {
--- src/components/SearchResultsList.svelte
+++ src/components/SearchResultsList.svelte
@@ -1,47 +1,27 @@
-<script></script>
+<script>
+	export let movies;
+</script>
 
 <div class="top absolute left-0 top-16 w-full rounded-lg bg-white/80 px-2 py-4">
 	<ul class="mr-4 max-h-[25vh] w-full overflow-y-auto">
-		<li class=" border-b border-slate-200">
-			<a href="/" class="flex items-center space-x-4 py-2">
-				<img
-					src="https://picsum.photos/100/200"
-					class="h-16 w-12 object-cover"
-					alt="movie thumbnail"
-				/>
-				<span class="font-semibold text-slate-700">Spider-Man: Homecoming</span>
-			</a>
-		</li>
-		<li class=" border-b border-slate-200">
-			<a href="/" class="flex items-center space-x-4 py-2">
-				<img
-					src="https://picsum.photos/100/200"
-					class="h-16 w-12 object-cover"
-					alt="movie thumbnail"
-				/>
-				<span class="font-semibold text-slate-700">Spider-Man: Homecoming</span>
-			</a>
-		</li>
-		<li class=" border-b border-slate-200">
-			<a href="/" class="flex items-center space-x-4 py-2">
-				<img
-					src="https://picsum.photos/100/200"
-					class="h-16 w-12 object-cover"
-					alt="movie thumbnail"
-				/>
-				<span class="font-semibold text-slate-700">Spider-Man: Homecoming</span>
-			</a>
-		</li>
-		<li class=" border-b border-slate-200">
-			<a href="/" class="flex items-center space-x-4 py-2">
-				<img
-					src="https://picsum.photos/100/200"
-					class="h-16 w-12 object-cover"
-					alt="movie thumbnail"
-				/>
-				<span class="font-semibold text-slate-700">Spider-Man: Homecoming</span>
-			</a>
-		</li>
+		{#if movies.length > 0}
+			{#each movies as movie}
+				<li class=" border-b border-slate-200">
+					<a href="/" target="_blank" class="flex items-center space-x-4 py-2">
+						<img
+							src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+							class="h-16 w-12 object-cover"
+							alt="movie thumbnail"
+						/>
+						<span class="font-semibold text-slate-700">{movie.title}</span>
+					</a>
+				</li>
+			{/each}
+		{:else}
+			<li>
+				<span class="font-semibold text-slate-700">No results found</span>
+			</li>
+		{/if}
 	</ul>
 </div>
 
